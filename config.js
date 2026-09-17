5f5246537e62af330b8000721c72f01ca7a76d7b
Davi <josedaviocs288@gmail.com>
2026-09-17 10:33:36 -0300

    Configura Supabase do FisioLink

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Preencha com os dados do seu projeto Supabase.
 // Supabase > Project Settings > API
 window.FISIOLINK_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_SUA_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "COLE_AQUI_SUA_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://qvbokscqoveoemagxgqs.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable__o8Qul-CFhJTbZoNOorwjw_fjW1vX-m"
 };
